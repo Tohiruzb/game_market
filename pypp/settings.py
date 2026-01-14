b183da4c93abf3a674038f63750de9ab0d2c2ffd
Tohir <tohiribrogimov86@gmail.com>
2026-01-14 17:39:57 +0500

    ozgarish

--- pypp/settings.py
+++ pypp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b+tw5t$ipkvhl+gr9jwv42k7t-gt63v%=l&tg#41-l@%%u_7*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-da113.up.railway.app']
 
 
 # Application definition
